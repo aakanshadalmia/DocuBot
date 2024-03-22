8dc79bcb0caf2d4c055cf9d6e0f14eb816d9235e
Aakansha Dalmia <aakanshadalmia@gmail.com>
2024-03-22 23:51:56 +0530

    Updated app

--- Pages/📄File Upload.py
+++ Pages/📄File Upload.py
@@ -7,13 +7,17 @@ import pdb
 st.set_page_config(layout="wide")
 
 def upload_documents():
-    st.title("Upload Documents")
-    st.write("We are currently accepting pdf files only")
+
+    st.write("<h1 style='text-align: center;'>Document Upload</h1>", unsafe_allow_html=True)
+    st.write("")
+    st.write("")
+    st.write("Have a large document that you want to retreive information from? Upload them here and head to the chatbot to get your queries answered!")    
 
     file_names = []
     data_store = DataStore()
 
-    uploaded_files = st.file_uploader("Upload one or more documents", accept_multiple_files=True)    
+    st.write("Please Note: We are currently accepting pdf files only")
+    uploaded_files = st.file_uploader("Upload here", accept_multiple_files=True)        
     all_uploaded_file_names = st.session_state.get("uploaded_files", set())    
 
     for file in uploaded_files:
@@ -21,6 +25,10 @@ def upload_documents():
 
     newly_uploaded_file_names = set(file_names) - set(all_uploaded_file_names)
     
+    if uploaded_files:
+        message_placeholder = st.empty()  # Create an empty placeholder for the message
+        message_placeholder.write("Your files are being uploaded, please wait...")  # Display message while files are uploading
+
     for file in uploaded_files:
         if file.name not in newly_uploaded_file_names:
             continue
@@ -35,9 +43,14 @@ def upload_documents():
             page_object = pdf_reader.pages[page_num]
             text = page_object.extract_text()
             data_store.ingest(text)
-        
+           
     st.session_state["uploaded_files"] = st.session_state.get("uploaded_files", set())
     st.session_state["uploaded_files"].update(newly_uploaded_file_names)
-        
+    
+    if newly_uploaded_file_names:
+        message_placeholder.empty()
+        st.write("")
+        st.write("<h3 style='text-align: center;'>Your files have been uploaded, DocuBot is ready to answer your questions now!</h3>", unsafe_allow_html = True)
+                    
 if __name__ == "__main__":
     upload_documents()    
